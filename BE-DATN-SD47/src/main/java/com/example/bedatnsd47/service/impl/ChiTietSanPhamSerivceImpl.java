f9f4111b6005ea2de5951312add4d104caa8f9c9
LAPTOP-NTA\hiepn <anhntph27418@fpt.edu.vn>
2023-10-25 00:58:52 +0700

    update ctsp

--- BE-DATN-SD47/src/main/java/com/example/bedatnsd47/service/impl/ChiTietSanPhamSerivceImpl.java
+++ BE-DATN-SD47/src/main/java/com/example/bedatnsd47/service/impl/ChiTietSanPhamSerivceImpl.java
@@ -17,36 +17,49 @@ public class ChiTietSanPhamSerivceImpl implements ChiTietSanPhamSerivce {
 
     @Override
     public List<ChiTietSanPham> getAll() {
+
         return repository.findAll();
+
     }
 
     @Override
     public ChiTietSanPham add(ChiTietSanPham sanPham) {
+
         return repository.save(sanPham);
+
     }
 
     @Override
     public ChiTietSanPham update(ChiTietSanPham sanPham) {
+
         return repository.save(sanPham);
+
     }
 
     @Override
     public void remove(Long id) {
+
         repository.deleteById(id);
+
     }
 
     @Override
     public ChiTietSanPham getById(Long id) {
+
         return repository.findById(id).get();
+
     }
 
     @Override
     public Page<ChiTietSanPham> getPage(Integer pageNo) {
+
         return repository.findAll(PageRequest.of(pageNo,5));
+
     }
 
     @Override
     public Integer checkPageNo(Integer pageNo) {
+
         Integer sizeList = repository.findAll().size();
         Integer pageCount = (int) Math.ceil((double) sizeList / 5);
         if (pageNo >= pageCount) {
@@ -55,15 +68,21 @@ public class ChiTietSanPhamSerivceImpl implements ChiTietSanPhamSerivce {
             pageNo = pageCount - 1;
         }
         return pageNo;
+
     }
 
     @Override
     public boolean checkTenTrung(String ten) {
+
         return false;
+
     }
 
     @Override
     public boolean checkTenTrungSua(String ma, String ten) {
+
         return false;
+
     }
+
 }
