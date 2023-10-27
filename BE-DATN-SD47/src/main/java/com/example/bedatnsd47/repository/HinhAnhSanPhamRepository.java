897932d35eafdaf021afebd8706c1e890feef894
LAPTOP-NTA\hiepn <anhntph27418@fpt.edu.vn>
2023-10-27 23:53:52 +0700

    update hình ảnh

---
+++ BE-DATN-SD47/src/main/java/com/example/bedatnsd47/repository/HinhAnhSanPhamRepository.java
@@ -0,0 +1,7 @@
+package com.example.bedatnsd47.repository;
+
+import com.example.bedatnsd47.entity.HinhAnhSanPham;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HinhAnhSanPhamRepository extends JpaRepository<HinhAnhSanPham,Long> {
+}
