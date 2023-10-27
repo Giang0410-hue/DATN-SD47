0b304782e58032f7c427e8ee6a5e87641c9c9225
DESKTOP-STQEMGH\longnh247 <longnhph27503@gmail.com>
2023-10-27 21:05:19 +0700

    loadsp

--- BE-DATN-SD47/src/main/java/com/example/bedatnsd47/controller/BanHangController.java
+++ BE-DATN-SD47/src/main/java/com/example/bedatnsd47/controller/BanHangController.java
@@ -1,6 +1,9 @@
 package com.example.bedatnsd47.controller;
 
+import com.example.bedatnsd47.entity.ChiTietSanPham;
 import com.example.bedatnsd47.entity.HoaDon;
+import com.example.bedatnsd47.entity.HoaDonChiTiet;
+import com.example.bedatnsd47.service.ChiTietSanPhamSerivce;
 import com.example.bedatnsd47.service.HoaDonChiTietService;
 import com.example.bedatnsd47.service.HoaDonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,36 +18,35 @@ public class BanHangController {
     @Autowired
     HoaDonService hoaDonService;
 
-     @Autowired
-     HoaDonChiTietService hoaDonChiTietService;
+    @Autowired
+    HoaDonChiTietService hoaDonChiTietService;
 
-    
-
+    @Autowired
+    ChiTietSanPhamSerivce chiTietSanPhamSerivce;
 
     @GetMapping("/home")
-    public String home(Model model){
-        model.addAttribute("lstHoaDon",hoaDonService.findAll());
-        model.addAttribute("lstHdct",hoaDonChiTietService.findByIdHoaDon(36l));
-        hoaDonService.findAll().forEach(o->{
-            System.out.println(o.getLstHoaDonChiTiet().toString());;
-
-        });
+    public String home(Model model) {
+        model.addAttribute("lstHoaDon", hoaDonService.findAll());
+        model.addAttribute("lstHdct", hoaDonChiTietService.findByIdHoaDon(36l));
+        model.addAttribute("lstCtsp", chiTietSanPhamSerivce.getAll());
         return "/admin-template/ban-hang-admin.html";
     }
 
     @PostMapping("/create")
-    public String taoHoaDon(){
+    public String taoHoaDon() {
         HoaDon hd = new HoaDon();
-        Integer size = hoaDonService.findAll().size()+1;
-        hd.setMaHoaDon("HD"+size);
+        Integer size = hoaDonService.findAll().size() + 1;
+        hd.setMaHoaDon("HD" + size);
         hoaDonService.saveOrUpdate(hd);
         return "redirect:home";
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         hoaDonService.deleteById(id);
-        return "redirect:home";
+        return "redirect:/ban-hang-tai-quay/home";
     }
+
+   
 
 }
