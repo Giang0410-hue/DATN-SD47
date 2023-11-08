39df68b05be8b628ac32a036854310ac4792b836
thienc45 <doanducthien123@gmail>
2023-11-08 15:07:30 +0700

    Revert "no message"
    This reverts commit 6a04b0a64e3d2e7a84424e4543cf6962c10640b7.

--- BE-DATN-SD47/src/main/java/com/example/bedatnsd47/entity/TaiKhoan.java
+++ BE-DATN-SD47/src/main/java/com/example/bedatnsd47/entity/TaiKhoan.java
@@ -15,7 +15,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -30,7 +29,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Entity
 @Table(name = "tai_khoan")
-@Builder
+
 public class TaiKhoan {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
